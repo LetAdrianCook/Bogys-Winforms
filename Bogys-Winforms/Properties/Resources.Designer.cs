86ad00dcab9ee4885528b4477ed149fd0b2157af
LetAdrianCook <adrianvillasis2252@gmail.com>
2025-04-15 16:11:57 +0800

    register page

--- Bogys-Winforms/Properties/Resources.Designer.cs
+++ Bogys-Winforms/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Bogys_Winforms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Bogsy_Logo {
+            get {
+                object obj = ResourceManager.GetObject("Bogsy-Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icons8_search_30 {
             get {
                 object obj = ResourceManager.GetObject("icons8-search-30", resourceCulture);
---
+++ Bogys-Winforms/Register.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Bogys_Winforms
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            textBox4 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox7 = new TextBox();
+            SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.FromArgb(168, 160, 192);
+            linkLabel1.Location = new Point(237, 102);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(54, 25);
+            linkLabel1.TabIndex = 10;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Login";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(109, 84, 181);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(37, 358);
+            button1.Name = "button1";
+            button1.Size = new Size(319, 50);
+            button1.TabIndex = 9;
+            button1.Text = "Create your account";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(60, 54, 76);
+            textBox3.BorderStyle = BorderStyle.FixedSingle;
+            textBox3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.ForeColor = Color.White;
+            textBox3.Location = new Point(37, 295);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "Confirm Password";
+            textBox3.Size = new Size(319, 34);
+            textBox3.TabIndex = 6;
+            textBox3.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(60, 54, 76);
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(37, 232);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(319, 34);
+            textBox2.TabIndex = 7;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(60, 54, 76);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(37, 165);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(319, 34);
+            textBox1.TabIndex = 8;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(37, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(208, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Already have an account?";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(320, 45);
+            label1.TabIndex = 5;
+            label1.Text = "Create your account";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.FromArgb(60, 54, 76);
+            textBox4.BorderStyle = BorderStyle.FixedSingle;
+            textBox4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox4.ForeColor = Color.White;
+            textBox4.Location = new Point(434, 165);
+            textBox4.Name = "textBox4";
+            textBox4.PlaceholderText = "First Name";
+            textBox4.Size = new Size(153, 34);
+            textBox4.TabIndex = 8;
+            textBox4.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            textBox6.BackColor = Color.FromArgb(60, 54, 76);
+            textBox6.BorderStyle = BorderStyle.FixedSingle;
+            textBox6.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox6.ForeColor = Color.White;
+            textBox6.Location = new Point(434, 232);
+            textBox6.Name = "textBox6";
+            textBox6.PlaceholderText = "Address";
+            textBox6.Size = new Size(319, 34);
+            textBox6.TabIndex = 7;
+            textBox6.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.FromArgb(60, 54, 76);
+            textBox5.BorderStyle = BorderStyle.FixedSingle;
+            textBox5.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox5.ForeColor = Color.White;
+            textBox5.Location = new Point(600, 165);
+            textBox5.Name = "textBox5";
+            textBox5.PlaceholderText = "Last Name";
+            textBox5.Size = new Size(153, 34);
+            textBox5.TabIndex = 8;
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            textBox7.BackColor = Color.FromArgb(60, 54, 76);
+            textBox7.BorderStyle = BorderStyle.FixedSingle;
+            textBox7.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox7.ForeColor = Color.White;
+            textBox7.Location = new Point(434, 295);
+            textBox7.Name = "textBox7";
+            textBox7.PlaceholderText = "MM / DD / YYYY";
+            textBox7.Size = new Size(319, 34);
+            textBox7.TabIndex = 7;
+            textBox7.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Register
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(44, 38, 56);
+            ClientSize = new Size(800, 450);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox2);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Register";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Register";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox7;
+    }
+}
