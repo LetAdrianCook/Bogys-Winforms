4da2e396a0782a6426d570d9ef8fd9c4a4e0ad84
LetAdrianCook <adrianvillasis2252@gmail.com>
2025-04-15 13:21:09 +0800

    Login form front end

--- Bogys-Winforms/Login.Designer.cs
+++ Bogys-Winforms/Login.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            label2 = new Label();
+            linkLabel1 = new LinkLabel();
+            panel1 = new Panel();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(439, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(349, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Login to your account";
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(60, 54, 76);
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
+            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.ForeColor = Color.White;
+            textBox1.Location = new Point(439, 155);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(319, 34);
+            textBox1.TabIndex = 1;
+            textBox1.TextAlign = HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(60, 54, 76);
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
+            textBox2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.ForeColor = Color.White;
+            textBox2.Location = new Point(439, 222);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(319, 34);
+            textBox2.TabIndex = 1;
+            textBox2.TextAlign = HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(109, 84, 181);
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(439, 284);
+            button1.Name = "button1";
+            button1.Size = new Size(319, 50);
+            button1.TabIndex = 2;
+            button1.Text = "Login your account";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(439, 92);
+            label2.Name = "label2";
+            label2.Size = new Size(192, 25);
+            label2.TabIndex = 0;
+            label2.Text = "Don't have an account?";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.FromArgb(168, 160, 192);
+            linkLabel1.Location = new Point(621, 92);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(72, 25);
+            linkLabel1.TabIndex = 3;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Register";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(395, 426);
+            panel1.TabIndex = 4;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(44, 38, 56);
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(linkLabel1);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = Color.White;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Login";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Label label2;
+        private LinkLabel linkLabel1;
+        private Panel panel1;
     }
 }
