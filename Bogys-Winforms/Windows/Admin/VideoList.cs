1a6a0f76b6869270cc2a7301348c006deaaea615
LetAdrianCook <adrianvillasis2252@gmail.com>
2025-04-30 16:45:03 +0800

    video List rent table loaded in the datagridview

--- Bogys-Winforms/Windows/Admin/VideoList.cs
+++ Bogys-Winforms/Windows/Admin/VideoList.cs
@@ -15,6 +15,43 @@ namespace Bogys_Winforms.Windows.Admin
         public VideoList()
         {
             InitializeComponent();
+            LoadVideosRented();
+        }
+        private void LoadVideosRented()
+        {
+            using (var context = new AppDbContext())
+            {
+                var rent = context.Rent.Select(r => new
+                {
+
+                    r.UserID,
+                    r.VideoID,
+                    r.VideoTitle,
+                    r.VideoCategory,
+                    r.RentDays,
+                    r.OverdueFee,
+                    r.Total,
+                    r.RentDate,
+                    r.ReturnDate,
+                    r.Status
+                }).ToList();
+
+                VideoView.DataSource = rent;
+                SetVideoColumnHeader();
+            }
+        }
+        private void SetVideoColumnHeader()
+        {
+            VideoView.Columns["UserID"].HeaderText = "User ID";
+            VideoView.Columns["VideoID"].HeaderText = "Video ID ";
+            VideoView.Columns["VideoTitle"].HeaderText = "Title";
+            VideoView.Columns["VideoCategory"].HeaderText = "Category";
+            VideoView.Columns["RentDays"].HeaderText = "Available Stock";
+            VideoView.Columns["OverdueFee"].HeaderText = "Overdue Fee";
+            VideoView.Columns["Total"].HeaderText = "Total";
+            VideoView.Columns["RentDate"].HeaderText = "Rental Date";
+            VideoView.Columns["ReturnDate"].HeaderText = "Return Date";
+            VideoView.Columns["Status"].HeaderText = "Status";
         }
     }
 }
